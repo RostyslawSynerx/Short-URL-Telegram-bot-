94cc563fd7440ce77c5bb98d1d29ea42f6108d1e
Rostyslaw <152879689+RostyslawSynerx@users.noreply.github.com>
2024-06-21 21:29:53 +0200

    main.py

--- main.py
+++ main.py
@@ -1,20 +1,18 @@
 import telebot
 import pyshorteners
 
-token = '6844540513:AAEj0H96Aa9_t2F4527rZpnITgWx6n32UlU'
+token = 'TOKEN'
 bot = telebot.TeleBot(token)
 
 @bot.message_handler(commands=['start'])
 def start(message):
     bot.send_message(message.chat.id, f'Отправьте ссылку и бот сделает её короче.\nSend a link and the bot will make it shorter.')
-    bot.send_message('1329613449', message.from_user.username)
 
 @bot.message_handler(func=lambda message: True)
 def url(message):
     pys = pyshorteners.Shortener()
     short_url = pys.tinyurl.short(message.text)
     bot.send_message(message.chat.id, short_url)
-
 
 bot.infinity_polling()
 
